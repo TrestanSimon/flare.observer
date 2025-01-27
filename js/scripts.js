b3fe9f5c4b36b1b8509d882d0fe0051e4ed15031
Trestan Simon <trestansimon@proton.me>
2025-01-27 00:50:55 -0700

    Use last commit time

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-	document.getElementById('commit-date').innerHTML = new Date(document.lastModified).toISOString().slice(0, -5)+'Z'
+	fetch('https://api.github.com/repos/TrestanSimon/flare.observer/commits/main')
+	.then(res => res.json())
+	.then(out =>
+		document.getElementById('commit-date').innerHTML = new Date(out.commit.author.date).toISOString().slice(0, -5)+'Z')
+	.catch(err => console.log(err));
 }
